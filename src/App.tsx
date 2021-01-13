66d10436175321e77e60fad95d26185e9e1c6373
Thiago Frois <zthiagopf@gmail.com>
2021-01-13 15:40:45 -0300

    added google anaylitics support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
+import Analytics from 'react-router-ga';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <>
     <BrowserRouter>
-      <Routes />
+      <Analytics id="G-8W2B7M8DFD">
+        <Routes />
+      </Analytics>
     </BrowserRouter>
   </>
 );
