677a46825472a38987abd2cd86fa3f80790f04e7
Thiago Frois <zthiagopf@gmail.com>
2021-06-30 13:53:17 -0300

    add help page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Dashboard from '../pages/Dashboard';
+import Help from '../pages/Help';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/categories/:category" component={Dashboard} />
+    <Route path="/help" component={Help} />
   </Switch>
 );
 
